d227a3d35b59817a3131c0abb3df69848aa8c924
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-11 09:03:43 -0500

    push

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::video::WindowPos;
 use std::time::Duration;
 
@@ -10,7 +11,7 @@ pub fn main() -> Result<(), String> {
 
   let mut window = video_subsystem
     .window("rust-sdl2 demo: Video", 2560, 1440)
-    .position_centered()
+    // .position_centered()
     .opengl()
     .build()
     .map_err(|e| e.to_string())?;
@@ -21,9 +22,10 @@ pub fn main() -> Result<(), String> {
 
   let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-  canvas.set_draw_color(Color::RGB(255, 0, 0));
-  canvas.clear();
-  canvas.present();
+  // canvas.set_draw_color(Color::RGB(0, 0, 0));
+  // canvas.clear();
+  // canvas.present();
+
   let mut event_pump = sdl_context.event_pump()?;
 
   'running: loop {
@@ -39,7 +41,12 @@ pub fn main() -> Result<(), String> {
     }
 
     canvas.clear();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 200));
+    canvas.fill_rect(Rect::new(0, 0, 100, 100))?;
+
     canvas.present();
+
     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
     // The rest of the game loop goes here...
   }
