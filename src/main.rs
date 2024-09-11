7059594c7dec7207be156067bd22c21d0dc086b2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-11 11:39:41 -0500

    Use bevvy

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,92 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::video::WindowPos;
-use std::time::Duration;
+//! Shows how to display a window in transparent mode.
+//!
+//! This feature works as expected depending on the platform. Please check the
+//! [documentation](https://docs.rs/bevy/latest/bevy/prelude/struct.Window.html#structfield.transparent)
+//! for more details.
 
-pub fn main() -> Result<(), String> {
-  let sdl_context = sdl2::init()?;
-  let video_subsystem = sdl_context.video()?;
+#[cfg(target_os = "macos")]
+use bevy::window::CompositeAlphaMode;
+use bevy::{
+  prelude::*,
+  sprite::{MaterialMesh2dBundle, Mesh2dHandle}, window::{Cursor, WindowLevel},
+};
 
-  let mut window = video_subsystem
-    .window("rust-sdl2 demo: Video", 2560, 1440)
-    // .position_centered()
-    .opengl()
-    .build()
-    .map_err(|e| e.to_string())?;
+fn main() {
+  App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+      primary_window: Some(Window {
+        // Setting `transparent` allows the `ClearColor`'s alpha value to take effect
+        transparent: true, // Disabling window decorations to make it feel more like a widget than a window
+        window_level: WindowLevel::AlwaysOnTop,
+        cursor: Cursor {
+          // Allow inputs to pass through to apps behind this app.
+          hit_test: false,
+          ..default()
+        },
+        decorations: false,
+        #[cfg(target_os = "macos")]
+        composite_alpha_mode: CompositeAlphaMode::PostMultiplied,
+        ..default()
+      }),
+      ..default()
+    }))
+    // ClearColor must have 0 alpha, otherwise some color will bleed through
+    .insert_resource(ClearColor(Color::NONE))
+    .add_systems(Startup, setup)
+    .run();
+}
+const X_EXTENT: f32 = 900.;
+fn setup(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+  commands.spawn(Camera2dBundle::default());
 
-  window.set_bordered(false);
-  window.set_always_on_top(true);
-  window.set_position(WindowPos::Positioned(0), WindowPos::Positioned(0));
+  let shapes = [
+    Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
+    Mesh2dHandle(meshes.add(CircularSector::new(50.0, 1.0))),
+    Mesh2dHandle(meshes.add(CircularSegment::new(50.0, 1.25))),
+    Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
+    Mesh2dHandle(meshes.add(Annulus::new(25.0, 50.0))),
+    Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
+    Mesh2dHandle(meshes.add(Rhombus::new(75.0, 100.0))),
+    Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
+    Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
+    Mesh2dHandle(meshes.add(Triangle2d::new(
+      Vec2::Y * 50.0,
+      Vec2::new(-50.0, -50.0),
+      Vec2::new(50.0, -50.0),
+    ))),
+  ];
+  let num_shapes = shapes.len();
 
-  let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+  for (i, shape) in shapes.into_iter().enumerate() {
+    // Distribute colors evenly across the rainbow.
+    let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
 
-  let mut x = 0;
-  let mut event_pump = sdl_context.event_pump()?;
-
-  'running: loop {
-    for event in event_pump.poll_iter() {
-      match event {
-        Event::Quit { .. }
-        | Event::KeyDown {
-          keycode: Some(Keycode::Escape),
-          ..
-        } => break 'running,
-        _ => {}
-      }
-    }
-
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-
-    canvas.set_draw_color(Color::RGB(0, 0, 200));
-    canvas.fill_rect(Rect::new(x, 0, 100, 100))?;
-    canvas.present();
-
-    x += 1;
-    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-    // The rest of the game loop goes here...
+    commands.spawn(MaterialMesh2dBundle {
+      mesh: shape,
+      material: materials.add(color),
+      transform: Transform::from_xyz(
+        // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+        -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+        0.0,
+        0.0,
+      ),
+      ..default()
+    });
   }
 
-  Ok(())
+  #[cfg(not(target_arch = "wasm32"))]
+  commands.spawn(
+    TextBundle::from_section("Press space to toggle wireframes", TextStyle::default()).with_style(
+      Style {
+        position_type: PositionType::Absolute,
+        top: Val::Px(12.0),
+        left: Val::Px(12.0),
+        ..default()
+      },
+    ),
+  );
 }
