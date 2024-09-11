9093fecaca57aff895866c579f7e1eaa63b5806f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-11 11:51:24 -0500

    ok

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@
 use bevy::window::CompositeAlphaMode;
 use bevy::{
   prelude::*,
-  sprite::{MaterialMesh2dBundle, Mesh2dHandle}, window::{Cursor, WindowLevel},
+  sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+  window::{Cursor, WindowLevel},
 };
 
 fn main() {
@@ -33,9 +34,16 @@ fn main() {
     // ClearColor must have 0 alpha, otherwise some color will bleed through
     .insert_resource(ClearColor(Color::NONE))
     .add_systems(Startup, setup)
+    .add_systems(Update, on_render)
     .run();
 }
 const X_EXTENT: f32 = 900.;
+
+fn on_render() {
+  // render 
+  println!("render");
+}
+
 fn setup(
   mut commands: Commands,
   mut meshes: ResMut<Assets<Mesh>>,
@@ -43,40 +51,22 @@ fn setup(
 ) {
   commands.spawn(Camera2dBundle::default());
 
-  let shapes = [
-    Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-    Mesh2dHandle(meshes.add(CircularSector::new(50.0, 1.0))),
-    Mesh2dHandle(meshes.add(CircularSegment::new(50.0, 1.25))),
-    Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-    Mesh2dHandle(meshes.add(Annulus::new(25.0, 50.0))),
-    Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-    Mesh2dHandle(meshes.add(Rhombus::new(75.0, 100.0))),
-    Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-    Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-    Mesh2dHandle(meshes.add(Triangle2d::new(
-      Vec2::Y * 50.0,
-      Vec2::new(-50.0, -50.0),
-      Vec2::new(50.0, -50.0),
-    ))),
-  ];
-  let num_shapes = shapes.len();
+  let square = Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0)));
 
-  for (i, shape) in shapes.into_iter().enumerate() {
-    // Distribute colors evenly across the rainbow.
-    let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+  // Distribute colors evenly across the rainbow.
+  let color = Color::hsl(0.0, 0.95, 0.7);
 
-    commands.spawn(MaterialMesh2dBundle {
-      mesh: shape,
-      material: materials.add(color),
-      transform: Transform::from_xyz(
-        // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-        -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-        0.0,
-        0.0,
-      ),
-      ..default()
-    });
-  }
+  commands.spawn(MaterialMesh2dBundle {
+    mesh: square,
+    material: materials.add(color),
+    transform: Transform::from_xyz(
+      // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+      -X_EXTENT / 2.,
+      0.0,
+      0.0,
+    ),
+    ..default()
+  });
 
   #[cfg(not(target_arch = "wasm32"))]
   commands.spawn(
